f70bedb81446857e4a41ffaf9d031fb9cbcfb60c
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-06-24 00:17:13 +0300

    171 finish
    171 finish

---
+++ components/form-submit.js
@@ -0,0 +1,15 @@
+"use client"
+import { useFormStatus } from "react-dom";
+
+export default function FormSubmit() {
+    const status = useFormStatus();
+
+    console.log(status);
+    if (status.pending) {
+        return <p>Creating Post....</p>
+    }
+    return <>
+        <button type="reset">Reset</button>
+        <button>Create Post</button>
+    </>
+}
