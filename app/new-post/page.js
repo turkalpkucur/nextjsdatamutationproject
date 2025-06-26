8a27304c3e125ec4a37770370bd9c83842a0ede5
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-06-26 03:31:39 +0300

    [173 FINISH]
    [173 FINISH]

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,14 +1,36 @@
+"use client"
 import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
+import { useActionState } from "react";
+
 
 export default function NewPostPage() {
+
+
 
   async function createPost(formData) {
     "use server";
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+
+    let errors = [];
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required.");
+    }
+
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required.");
+    }
+
+    if (!image) {
+      errors.push("Image is required.");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
 
     await storePost({
       imageUrl: '',
@@ -22,13 +44,15 @@ export default function NewPostPage() {
     redirect('feed');
 
   }
+
+  const [state, formAction] = useActionState(createPost, {})
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title" required />
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
