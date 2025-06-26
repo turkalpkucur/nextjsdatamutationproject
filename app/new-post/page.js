9df8320ab26c566dc740d7c4aecfdf7df2483f22
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-06-26 23:29:52 +0300

    [174 FINISH]
    [174 FINISH]

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,15 +1,16 @@
-"use client"
-import FormSubmit from "@/components/form-submit";
+
+
+import PostForm from "@/components/post-form";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
-import { useActionState } from "react";
+
 
 
 export default function NewPostPage() {
 
 
 
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server";
     const title = formData.get('title');
     const image = formData.get('image');
@@ -24,7 +25,7 @@ export default function NewPostPage() {
       errors.push("Content is required.");
     }
 
-    if (!image) {
+    if (!image || image.size === 0) {
       errors.push("Image is required.");
     }
 
@@ -40,37 +41,8 @@ export default function NewPostPage() {
 
     });
 
-
     redirect('feed');
-
   }
 
-  const [state, formAction] = useActionState(createPost, {})
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={formAction}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" required />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />
 }
