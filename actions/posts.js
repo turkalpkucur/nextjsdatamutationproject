e2257db77466fcc4061ae75af6ab22093f319d67
Türkalp Kucur <turkalp.kucur@gmail.com>
2025-08-29 22:03:38 +0300

    güncelleme
    güncelleme

--- actions/posts.js
+++ actions/posts.js
@@ -1,6 +1,7 @@
 "use server";
 import { uploadImage } from "@/lib/cloudinary";
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 
@@ -47,5 +48,6 @@ export async function createPost(prevState, formData) {
 }
 
 export async function togglePostLikeStatus(postId) {
-    updatePostLikeStatus(postId, 2);
+    await updatePostLikeStatus(postId, 2);
+    revalidatePath('');
 }
